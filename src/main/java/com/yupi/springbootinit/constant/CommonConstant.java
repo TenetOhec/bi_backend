5bae5203f355cf86eec45e8b6589ed45470ee834
TenetOhec <511605181@qq.com>
2024-08-14 18:12:09 +0800

    full commit

---
+++ src/main/java/com/yupi/springbootinit/constant/CommonConstant.java
@@ -0,0 +1,25 @@
+package com.yupi.springbootinit.constant;
+
+/**
+ * 通用常量
+ *
+ * @author <a href="https://github.com/liyupi">zhuweimin</a>
+ * @from <a href="https://yupi.icu">hello，world</a>
+ */
+public interface CommonConstant {
+
+    /**
+     * 升序
+     */
+    String SORT_ORDER_ASC = "ascend";
+
+    /**
+     * 降序
+     */
+    String SORT_ORDER_DESC = " descend";
+
+    /**
+     * BI 模型 id
+     */
+    long BI_MODEL_ID = 1812040398431780866L;
+}
